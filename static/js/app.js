c454cbaafdcfb965ef232db133dad1d8eda3f913
nalchamp <nicholasalym@gmail.com>
2024-02-13 22:46:37 -0500

    Bar chart coding completed with change in between data

--- static/js/app.js
+++ static/js/app.js
@@ -76,3 +76,68 @@ function buildMetadata(sample) {
     });
 
 };
+
+// Function that builds the bar chart
+function buildBarChart(sample) {
+
+    // Use D3 to retrieve all of the data
+    d3.json(url).then((data) => {
+
+        // Retrieve all sample data
+        let sampleInfo = data.samples;
+
+        // Filter based on the value of the sample
+        let value = sampleInfo.filter(result => result.id == sample);
+
+        // Get the first index from the array
+        let valueData = value[0];
+
+        // Get the otu_ids, lables, and sample values
+        let otu_ids = valueData.otu_ids;
+        let otu_labels = valueData.otu_labels;
+        let sample_values = valueData.sample_values;
+
+        // Log the data to the console
+        console.log(otu_ids,otu_labels,sample_values);
+
+        // Set top ten items to display in descending order
+        let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
+        let xticks = sample_values.slice(0,10).reverse();
+        let labels = otu_labels.slice(0,10).reverse();
+        
+        // Set up the trace for the bar chart
+        let trace = {
+            x: xticks,
+            y: yticks,
+            text: labels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        // Setup the layout
+        let layout = {
+            title: "Top 10 OTUs Present"
+        };
+
+        // Call Plotly to plot the bar chart
+        Plotly.newPlot("bar", [trace], layout)
+    });
+};
+
+
+
+// Function that updates dashboard when sample is changed
+function optionChanged(value) { 
+
+    // Log the new value
+    console.log(value); 
+
+    // Call all functions 
+    buildMetadata(value);
+    buildBarChart(value);
+    buildBubbleChart(value);
+    buildGaugeChart(value);
+};
+
+// Call the initialize function
+init();
